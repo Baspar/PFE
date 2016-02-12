fc1d7b150adecc5c2536788b94f5fa2af36bc938
Blaine <bastien.laine@insa-rouen.fr>
2016-02-12 16:52:01 +0100

    [add] Finalisation MarkovNode+Document et debut KthMarkov

---
+++ src/AllKthMarkov/java/src/KthMarkov.java
@@ -0,0 +1,82 @@
+import java.util.Hashtable;
+import java.util.Vector;
+import java.util.Map;
+public class KthMarkov{
+    //Attributs
+    private Hashtable<Vector<Document>,MarkovNode> nodes;
+
+    //Constructeur
+    public KthMarkov(){//DONE
+        nodes=new Hashtable<Vector<Document>,MarkovNode>();
+    }
+
+    //Getter
+    public MarkovNode getDocs(Vector<Document> docs){//DONE
+        if(!nodes.contains(docs))
+            return null;
+        return nodes.get(docs);
+    }
+
+    //Methodes
+    public void addNewNode(Vector<Document> node, int poidsNode){//DONE
+        if(!nodes.contains(node)){
+            nodes.put(node, new MarkovNode(node));
+            nodes.get(node).setTotal(poidsNode);
+        }
+    }
+    public void renforcerChaine(Vector<Document> oldDocs, Document nextDoc){//DONE
+        //Creation du vecteur nouveau document
+        Vector<Document> newDocs= new Vector<Document>();
+        for(Document doc:oldDocs)
+            newDocs.add(doc);
+        newDocs.remove(0);
+        newDocs.add(nextDoc);
+
+        //Insertion de noeuds si ces derniers n'existent pas
+        if(!nodes.contains(oldDocs))
+            nodes.put(oldDocs, new MarkovNode(oldDocs));
+        if(!nodes.contains(newDocs))
+            nodes.put(newDocs, new MarkovNode(newDocs));
+
+        //Renforcement de la chaine
+        nodes.get(oldDocs).renforcerChaine(nodes.get(newDocs));
+    }
+    public String toSave(){//DONE
+        String out="";
+
+        //Nb noeud
+        out+=nodes.size();
+        out+=" ";
+
+        //Nb Liens
+        int nbLiens=0;
+        for(MarkovNode m:nodes.values())
+            nbLiens+=m.getNbChild();
+        out+=nbLiens;
+        out+="\n";
+
+        //Affichage Noeuds
+        for(Map.Entry<Vector<Document>, MarkovNode> node:nodes.entrySet()){
+            for(Document doc:node.getKey())
+                out += doc.getId()+" ";
+            out += "["+node.getValue().getTotal()+"]\n";
+        }
+
+        //Affichage liens
+        for(Map.Entry<Vector<Document>, MarkovNode> node : nodes.entrySet()){
+            for(Map.Entry<MarkovNode, Integer> child : node.getValue().getChild().entrySet()){
+                for(Document doc : node.getKey())
+                    out += doc.getId()+" ";
+                out += "-("+child.getValue()+")-";
+                for(Document doc : child.getKey().getMaChaineDoc())
+                    out += doc.getId()+" ";
+                out += "\n";
+            }
+        }
+        return out;
+    }
+    public String toUML(){//TODO
+        String out="";
+        return out;
+    }
+}
--- src/AllKthMarkov/java/src/MarkovNode.java
+++ src/AllKthMarkov/java/src/MarkovNode.java
@@ -3,21 +3,21 @@ import java.util.Hashtable;
 
 public class MarkovNode{
     //Parametres
-    private Vector<Integer> maChaineDoc;
+    private Vector<Document> maChaineDoc;
     private Hashtable<MarkovNode,Integer> child;
     private int total;
 
     //Constructeur
-    public MarkovNode(Vector<Integer> chaine){//DONE
+    public MarkovNode(Vector<Document> chaine){//DONE
         child=new Hashtable<MarkovNode, Integer>();
-        maChaineDoc=new Vector<Integer>();
+        maChaineDoc=new Vector<Document>();
 
         total=0;
         maChaineDoc.addAll(chaine);
     }
 
     //Methodes
-    public Integer getLastDoc(){//DONE
+    private Document getLastDoc(){//DONE
         return maChaineDoc.get(maChaineDoc.size()-1);
     }
     public void renforcerChaine(MarkovNode nextChaine){//DONE
@@ -31,18 +31,18 @@ public class MarkovNode{
         else
             child.put(nextChaine, old+1);
     }
-    public Vector<Pair<Integer,Float>> guessNextDocs(){//DONE
-        Vector<Pair<Integer,Float>> out=new Vector<Pair<Integer,Float>>();
+    public Vector<Pair<Document,Float>> guessNextDocs(){//DONE
+        Vector<Pair<Document,Float>> out=new Vector<Pair<Document,Float>>();
 
         for(MarkovNode node:child.keySet()){
             //Récupération ID document suivant
-            Integer idLastDoc=node.getLastDoc();
+            Document idLastDoc=node.getLastDoc();
 
             //Calcul probabilité
             Float prob=((float)child.get(node))/(float)total;
 
             //Ajout
-            out.add(new Pair<Integer, Float>(idLastDoc, prob));
+            out.add(new Pair<Document, Float>(idLastDoc, prob));
         }
 
         return out;
@@ -52,7 +52,24 @@ public class MarkovNode{
     }
 
     //Getter
-    public Vector<Integer> getMaChaineDoc(){//DONE
+    public Vector<Document> getMaChaineDoc(){//DONE
         return maChaineDoc;
+    }
+    public int getNbChild(){//DONE
+        return child.size();
+    }
+    public int getTotal(){//DONE
+        return this.total;
+    }
+    public Hashtable<MarkovNode,Integer> getChild(){//DONE
+        return this.child;
+    }
+
+    //Setter
+    public void setChild(MarkovNode markovNode, Integer nb){//DONE
+        child.put(markovNode, nb);
+    }
+    public void setTotal(int total){//DONE
+        this.total=total;
     }
 }
